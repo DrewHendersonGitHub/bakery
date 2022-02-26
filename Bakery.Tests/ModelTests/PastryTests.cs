78f4b8915c35d41a37e3fcc6b5cd628962c50373
Drew Henderson <drewhendersonemail@gmail.com>
2022-02-25 17:10:56 -0800

    add first test for Pastry

---
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+
+  public class PastryTests
+  {
+    [TestMethod]
+    public void Pastry_ConstructorTest_String()
+    {
+      Pastry newPastry = new Pastry(11);
+      Assert.AreEqual(11, newPastry.Amount);
+    }
+  }
+}
